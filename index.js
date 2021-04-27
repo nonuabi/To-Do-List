6a6fdbaa04c3a840ed537ac4c467a50af6d81e8d
nonuabi <nonuabi@gmail.com>
2021-04-27 19:50:22 +0530

    express is installed and server is working fine

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const port = 8000;
+const app = express();
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`error occure form the server : ${err}`);
+    return;
+  }
+  console.log(`Server Sucessfully run on port : ${port}`);
+});
